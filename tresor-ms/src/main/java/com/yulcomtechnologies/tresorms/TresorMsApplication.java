580aa2d39bdcaccba5b170f91e2ec64f53622376
kulturman <arnaudbakyono@gmail.com>
2024-09-21 20:31:23 +0000

    Init projects

---
+++ tresor-ms/src/main/java/com/yulcomtechnologies/tresorms/TresorMsApplication.java
@@ -0,0 +1,13 @@
+package com.yulcomtechnologies.tresorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TresorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TresorMsApplication.class, args);
+	}
+
+}
---
+++ tresor-ms/src/test/java/com/yulcomtechnologies/tresorms/TresorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yulcomtechnologies.tresorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TresorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
