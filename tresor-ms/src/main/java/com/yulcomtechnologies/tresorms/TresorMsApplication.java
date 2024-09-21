e1e99b081741940c3190766e00ab27b772c480ff
kulturman <arnaudbakyono@gmail.com>
2024-09-21 22:06:17 +0000

    Add projects file

--- tresor-ms/src/main/java/com/yulcomtechnologies/tresorms/TresorMsApplication.java
+++ tresor-ms/src/main/java/com/yulcomtechnologies/tresorms/TresorMsApplication.java
@@ -2,8 +2,10 @@ package com.yulcomtechnologies.tresorms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TresorMsApplication {
 
 	public static void main(String[] args) {
