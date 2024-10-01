f2d6ff8d3b5980d5262c0a13f86396df98cee2a7
kulturman <arnaudbakyono@gmail.com>
2024-10-01 14:31:42 +0000

    Init drtss MS

---
+++ drtss-ms/src/test/java/com/yulcomtechnologies/drtssms/DrtssMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yulcomtechnologies.drtssms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrtssMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
