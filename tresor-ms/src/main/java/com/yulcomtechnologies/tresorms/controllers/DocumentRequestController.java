00c625f73d8baa955be256257f7cc717057a5c49
kulturman <arnaudbakyono@gmail.com>
2024-10-22 19:35:25 +0000

    Fix swagger doc issue

--- tresor-ms/src/main/java/com/yulcomtechnologies/tresorms/controllers/DocumentRequestController.java
+++ tresor-ms/src/main/java/com/yulcomtechnologies/tresorms/controllers/DocumentRequestController.java
@@ -9,15 +9,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class DocumentRequestController {
     private final DocumentRequestService documentRequestService;
