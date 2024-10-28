9a4b286c1c49c1d082b43ff20017cfd210b83b45
kulturman <arnaudbakyono@gmail.com>
2024-10-28 00:14:04 +0000

    Update gateway

--- drtss-ms/src/main/java/com/yulcomtechnologies/drtssms/config/security/SecurityConfig.java
+++ drtss-ms/src/main/java/com/yulcomtechnologies/drtssms/config/security/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.yulcomtechnologies.drtssms.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,15 +13,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(req ->
                     req.anyRequest()
                     .permitAll()
-            );
+            )
             /*.oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt.decoder(JwtDecoders.fromIssuerLocation("http://localhost:9080/realms/myrealm")))
-            );*/
+            );*/;
         return http.build();
     }
 }
--- gateway/src/main/java/com/yulcomtechnologies/gateway/config/security/SecurityConfig.java
+++ gateway/src/main/java/com/yulcomtechnologies/gateway/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.yulcomtechnologies.gateway.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -14,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUri;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,11 +34,11 @@ public class SecurityConfig {
                         )
                         .permitAll()
                         .anyRequest()
-                .authenticated()
-            );
+                        .permitAll()
+            )
             /*.oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt.decoder(JwtDecoders.fromIssuerLocation("http://localhost:9080/realms/myrealm")))
-            );*/
+                .jwt(jwt -> jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri)))
+            );*/;
         return http.build();
     }
 }
