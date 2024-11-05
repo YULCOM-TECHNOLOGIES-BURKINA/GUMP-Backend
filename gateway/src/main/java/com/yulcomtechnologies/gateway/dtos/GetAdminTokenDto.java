1cd923e01708b47980e6f941b340848fe35fb229
kulturman <arnaudbakyono@gmail.com>
2024-11-05 01:36:01 +0000

    Fix gateway

--- gateway/src/main/java/com/yulcomtechnologies/gateway/dtos/GetAdminTokenDto.java
+++
@@ -1,6 +0,0 @@
-package com.yulcomtechnologies.gateway.dtos;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record GetAdminTokenDto(@JsonProperty("access_token") String accessToken) {
-}
--- gateway/src/main/java/com/yulcomtechnologies/gateway/services/CorporationInfosExtractor.java
+++
@@ -1,5 +0,0 @@
-package com.yulcomtechnologies.gateway.services;
-
-public interface CorporationInfosExtractor {
-    CorporationData extractCorporationInfos(String ifuNumber);
-}
