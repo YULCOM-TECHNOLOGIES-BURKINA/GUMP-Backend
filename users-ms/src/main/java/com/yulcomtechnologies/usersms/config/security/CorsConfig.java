6eec2f803af8ee380bffa28ea0f68efefadeeb84
kulturman <arnaudbakyono@gmail.com>
2024-11-14 23:29:22 +0000

    Fix cors

--- users-ms/src/main/java/com/yulcomtechnologies/usersms/config/security/CorsConfig.java
+++ users-ms/src/main/java/com/yulcomtechnologies/usersms/config/security/CorsConfig.java
@@ -8,11 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry
+            .addMapping("/**")
+            .allowedMethods("*")
             .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("Content-Type", "Authorization")
-            .allowCredentials(true)
-            .maxAge(3600);
+            .allowedHeaders("*");
     }
 }
