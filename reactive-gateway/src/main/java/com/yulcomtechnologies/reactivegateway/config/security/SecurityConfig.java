c1505539b89883687af152172fa754433ecb2e61
kulturman <arnaudbakyono@gmail.com>
2024-11-26 12:08:17 +0000

    Fix reactive gateway

--- reactive-gateway/src/main/java/com/yulcomtechnologies/reactivegateway/config/security/SecurityConfig.java
+++ reactive-gateway/src/main/java/com/yulcomtechnologies/reactivegateway/config/security/SecurityConfig.java
@@ -1,11 +1,11 @@
 package com.yulcomtechnologies.reactivegateway.config.security;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
@@ -15,33 +15,21 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-@EnableReactiveMethodSecurity // Enable method-level security for reactive environments
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuerUri;
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Configure CORS
-            .csrf(ServerHttpSecurity.CsrfSpec::disable) // Disable CSRF
-            .authorizeExchange(auth -> auth
-                .pathMatchers(
-                    "/auth/**",
-                    "/v2/api-docs",
-                    "/v3/api-docs/**",
-                    "/swagger-resources",
-                    "/swagger-resources/**",
-                    "/swagger-ui/**"
-                ).permitAll() // Permit specific endpoints
-                .anyExchange().authenticated() // Authenticate all other requests
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt.jwtDecoder(JwtDecoders.fromIssuerLocation(issuerUri))) // Configure JWT
-            );
-
-        return http.build();
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
+                .permitAll()
+                .anyExchange().authenticated()
+            ).oauth2ResourceServer((oauth) -> oauth
+                .jwt(Customizer.withDefaults()))
+            .build();
     }
 
     @Bean
