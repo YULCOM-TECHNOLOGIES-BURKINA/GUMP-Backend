a5503988ffb2909e9f7250f502cee63b150bd73e
mandson1er <mandson1er@gmail.com>
2024-12-15 20:21:36 +0000

    fix drtps Documents request file to  back

--- drtss-ms/src/main/java/com/yulcomtechnologies/drtssms/controllers/DocumentController.java
+++ drtss-ms/src/main/java/com/yulcomtechnologies/drtssms/controllers/DocumentController.java
@@ -4,7 +4,6 @@ import com.yulcomtechnologies.drtssms.dtos.*;
 import com.yulcomtechnologies.drtssms.entities.DocumentRequest;
 import com.yulcomtechnologies.drtssms.enums.DocumentRequestStatus;
 import com.yulcomtechnologies.drtssms.services.DocumentRequestService;
-import com.yulcomtechnologies.sharedlibrary.auth.AuthenticatedUserData;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,8 +14,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -25,17 +22,17 @@ public class DocumentController {
 
     @PostMapping("demandes/{id}/pay")
     public ResponseEntity<PaymentRequestResponse> payForRequest(
-        @PathVariable Long id,
-        @Validated @RequestBody PayRequest payRequest
-        ) {
+            @PathVariable Long id,
+            @Validated @RequestBody PayRequest payRequest
+    ) {
 
         return ResponseEntity.ok(documentRequestService.pay(id, payRequest));
     }
 
     @PostMapping("demandes/{id}/update-payment-status")
     public ResponseEntity<Void> updatePaymentStatus(
-        @PathVariable Long id,
-        @RequestParam String paymentId
+            @PathVariable Long id,
+            @RequestParam String paymentId
     ) {
         documentRequestService.updatePaymentStatus(id, paymentId);
         return ResponseEntity.ok().build();
@@ -43,7 +40,7 @@ public class DocumentController {
 
     @GetMapping("demandes/{id}/payment-status")
     public ResponseEntity<DocumentRequestPaymentStatus> getPaymentStatus(
-        @PathVariable Long id
+            @PathVariable Long id
     ) {
         var documentRequest = documentRequestService.getDocumentRequest(id.toString());
 
@@ -52,45 +49,30 @@ public class DocumentController {
 
     @PostMapping("demandes")
     public ResponseEntity<CreatedResource> submitDocumentRequest(
-        @RequestParam(value = "attestationCnss",required = false) MultipartFile attestationCnss,
-        @RequestParam(value = "attestationAnpe",required = false) MultipartFile attestationAnpe,
-        @RequestParam("publicContractNumber") String publicContractNumber,
-        @RequestParam(value = "contractPurpose", required = false) String contractPurpose,
-        @RequestParam(value = "contractingOrganizationName", required = false) String contractingOrganizationName,
-        @RequestParam(value = "isForPublicContract", required = false, defaultValue = "false") Boolean isForPublicContract,
-        @RequestParam(value = "email", required = true) String email
-
-        ) throws IOException {
+            @RequestParam("attestationCnss") MultipartFile attestationCnss,
+            @RequestParam("attestationAnpe") MultipartFile attestationAnpe,
+            @RequestParam("publicContractNumber") String publicContractNumber,
+            @RequestParam(value = "contractPurpose", required = false) String contractPurpose,
+            @RequestParam(value = "contractingOrganizationName", required = false) String contractingOrganizationName,
+            @RequestParam(value = "isForPublicContract", required = false, defaultValue = "false") Boolean isForPublicContract
+    ) throws IOException {
 
         DocumentRequest documentRequest = documentRequestService.submitDocumentRequest(
-            attestationCnss,
-            attestationAnpe,
-            publicContractNumber,
-            isForPublicContract,
-            contractPurpose,
-            contractingOrganizationName,
-             email
+                attestationCnss,
+                attestationAnpe,
+                publicContractNumber,
+                isForPublicContract,
+                contractPurpose,
+                contractingOrganizationName
         );
 
         return ResponseEntity.ok(new CreatedResource(documentRequest.getId().toString()));
     }
 
     @GetMapping("demandes")
-    public ResponseEntity<Page<DocumentRequestDto>> getDocumentRequests(Pageable pageable,String email) {
-        var documentRequests = documentRequestService.getPaginatedDocumentRequests(pageable,email);
+    public ResponseEntity<Page<DocumentRequestDto>> getDocumentRequests(Pageable pageable) {
+        var documentRequests = documentRequestService.getPaginatedDocumentRequests(pageable);
         return ResponseEntity.ok(documentRequests);
-    }
-
-    @GetMapping("demandes-test/{keycloakUserId}")
-    public ResponseEntity<Page<DocumentRequestDto>> getDocumentRequests2(Pageable pageable,@PathVariable String keycloakUserId) {
-        Optional<AuthenticatedUserData>  userData = documentRequestService.getUserDetails();
-        var documentRequests = documentRequestService.getPaginatedDocumentRequests2(pageable,userData.get().getKeycloakUserId());
-        return ResponseEntity.ok(documentRequests);
-    }
-
-   @GetMapping("authenticatedUsercontrole")
-    public AuthenticatedUserData authenticatedUsercontrole() {
-      return   documentRequestService.getUserDetails().get();
     }
 
     @GetMapping("demandes/{id}")
@@ -101,24 +83,20 @@ public class DocumentController {
 
     @PostMapping("demandes/{id}/review")
     public ResponseEntity<?> reviewDocumentRequest(
-        @PathVariable Long id,
-        @RequestParam("status") DocumentRequestStatus status,
-        @RequestParam(required = false) String rejectionReason,
-        @RequestParam(value = "email", required = true) String email
-
-        ) {
-        documentRequestService.reviewDocumentRequest(id, status, rejectionReason, email);
+            @PathVariable Long id,
+            @RequestParam("status") DocumentRequestStatus status,
+            @RequestParam(required = false) String rejectionReason
+    ) {
+        documentRequestService.reviewDocumentRequest(id, status, rejectionReason);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("demandes/{id}/approve")
     public ResponseEntity<?> approveDocumentRequest(
-        @PathVariable Long id,
-        @RequestBody @Validated ApproveDocumentRequestDto approveDocumentRequestDto,
-        @RequestParam(value = "email", required = true) String email
-
-        ) throws IOException {
-        documentRequestService.approveDocumentRequest(id, approveDocumentRequestDto,email);
+            @PathVariable Long id,
+            @RequestBody @Validated ApproveDocumentRequestDto approveDocumentRequestDto
+    ) throws IOException {
+        documentRequestService.approveDocumentRequest(id, approveDocumentRequestDto);
         return ResponseEntity.ok().build();
     }
 
