09cf494428a182c7e72dd4b375f26ad669514ec3
kulturman <arnaudbakyono@gmail.com>
2024-12-22 21:39:31 +0000

    Add error reporting on other services

---
+++ tresor-ms/src/main/java/com/yulcomtechnologies/tresorms/config/CustomErrorResponse.java
@@ -0,0 +1,14 @@
+package com.yulcomtechnologies.tresorms.config;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class CustomErrorResponse {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+    private List<Error> errors;
+}
