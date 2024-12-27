50edb691cedeb766b41cafcecb8593abb3c85e59
kulturman <arnaudbakyono@gmail.com>
2024-12-27 21:17:49 +0000

    Add validation on tresor UpdateApplicationConfigRequest

--- tresor-ms/src/main/java/com/yulcomtechnologies/tresorms/dtos/UpdateApplicationConfigRequest.java
+++ tresor-ms/src/main/java/com/yulcomtechnologies/tresorms/dtos/UpdateApplicationConfigRequest.java
@@ -1,18 +1,23 @@
 package com.yulcomtechnologies.tresorms.dtos;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UpdateApplicationConfigRequest {
+    @NotNull
     private Integer validityTimeInMonthsForLiquidation;
 
+    @NotNull
     private Integer validityTimeInMonthsForSoumission;
 
     private String header;
 
     private String footer;
 
+    @NotNull
     private Integer processingTimeInDaysForLiquidation;
 
+    @NotNull
     private Integer processingTimeInDaysForSoumission;
 }
